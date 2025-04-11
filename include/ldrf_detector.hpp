9f1bc1c02a44975f7a15181fa117a0eae7695b5f
Sam Richter <samrichter888@gmail.com>
2025-04-11 16:21:06 -0500

    ldrf grav

--- include/ldrf_detector.hpp
+++ include/ldrf_detector.hpp
@@ -112,6 +112,10 @@ public:
     DetectionStatus calculatePose(
         const pcl::PointCloud<PointT>& local_cloud,
         util::geom::Pose3f& pose );
+    DetectionStatus calculatePose(
+        const pcl::PointCloud<PointT>& local_cloud,
+        const Eigen::Vector3d& local_grav,
+        util::geom::Pose3f& pose );
 
     void applyParams(
         double point_range_thresh,
@@ -250,6 +254,170 @@ typename LidarFiducialDetector<PointT>::DetectionStatus LidarFiducialDetector<Po
     this->seg.setInputCloud(this->seg_cloud_ptrs[0]);
 
     for(size_t iter = 0; iter < 3; iter++)
+    {
+        this->seg_indices.indices.clear();
+        this->plane_coeffs.values.clear();
+        this->seg.segment(this->seg_indices, this->plane_coeffs);
+
+        if(this->seg_indices.indices.size() > this->param.num_seg_points_thresh)
+        {
+            if(iter < 2)
+            {
+                // copy the rest of the points to the next seg cloud
+                util::pc_copy_inverse_selection(
+                    this->seg_clouds[iter],
+                    this->seg_indices.indices,
+                    this->seg_clouds[iter + 1] );
+
+                this->seg.setInputCloud(this->seg_cloud_ptrs[iter + 1]);
+            }
+            else
+            {
+                util::pc_copy_inverse_selection(
+                    this->seg_clouds[iter],
+                    this->seg_indices.indices,
+                    this->leftover_cloud );
+            }
+
+            // each cloud ends up with only the segmented points
+            util::pc_normalize_selection(
+                this->seg_clouds[iter], this->seg_indices.indices );
+
+            this->seg_planes[iter] = Eigen::Vector4f{ this->plane_coeffs.values.data() };
+
+            switch(iter)
+            {
+                case 0:
+                {
+                    this->seg.setModelType(pcl::SACMODEL_PARALLEL_PLANE);
+                    this->seg.setAxis(this->seg_planes[0].head<3>());
+                    break;
+                }
+                case 1:
+                {
+                    this->seg.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
+                    this->seg.setAxis( this->seg_planes[0].cross3(this->seg_planes[1]).head<3>() );
+                    break;
+                }
+            }
+
+            status.iterations++;
+        }
+        else
+        {
+            return status;
+        }
+    }
+
+    status.has_seg_point_num = true;
+    status.has_remaining_point_num =
+        ((static_cast<double>(this->leftover_cloud.size()) / static_cast<double>(starting_num_points))
+            <= this->param.max_leftover_points_rel_thresh );
+
+    if(status)
+    {
+        // std::array<Eigen::Vector3f, 3> bbox_diffs;
+        // Eigen::Vector3f _min, _max;
+        // for(size_t i = 0; i < 3; i++)
+        // {
+        //     util::minMaxXYZ(this->seg_clouds[i], _min, _max);
+        //     bbox_diffs[i] = _max - _min;
+        // }
+
+        Eigen::Matrix3f rotation, hhr;
+        for(size_t i = 0; i < 3; i++)
+        {
+            this->plane_centers[i] = Eigen::Vector3f::Zero();
+            size_t num = 0;
+            for(const auto& pt : this->seg_clouds[i].points)
+            {
+                this->plane_centers[i] += pt.getVector3fMap();
+                num++;
+            }
+            this->plane_centers[i] /= static_cast<float>(num);
+
+            // vector from origin to plane center should be the opposite direction of the normal
+            if(this->plane_centers[i].dot(this->seg_planes[i].head<3>()) > 0.f)
+            {
+                this->seg_planes[i] *= -1.f;
+            }
+
+            rotation.block<1, 3>(i, 0) = this->seg_planes[i].head<3>();
+        }
+
+        auto hh = rotation.householderQr();
+        rotation = hh.householderQ();
+        hhr = hh.matrixQR().triangularView<Eigen::Upper>();
+        for(size_t i = 0; i < 3; i++)
+        {
+            if(hhr(i, i) < 0.f) rotation.block<3, 1>(0, i) *= -1.f;
+        }
+
+    #if LFD_USE_ORTHO_PLANE_INTERSECTION > 0
+        for(size_t i = 0; i < 3; i++)
+        {
+            this->seg_planes[i].head<3>() = rotation.block<1, 3>(i, 0);
+            this->seg_planes[i][3] = this->seg_planes[i].head<3>().dot(this->plane_centers[i]); // ax + by + cz = d --> (a, b, c)*(x, y, z) = d
+        }
+    #endif
+
+        pose.quat = Eigen::Quaternionf{ rotation }.inverse();
+        pcl::threePlanesIntersection(
+            this->seg_planes[0],
+            this->seg_planes[1],
+            this->seg_planes[2],
+            pose.vec );
+        pose.vec *= -1.f;
+    }
+
+    return status;
+}
+
+
+template<typename PointT>
+typename LidarFiducialDetector<PointT>::DetectionStatus LidarFiducialDetector<PointT>::calculatePose(
+    const pcl::PointCloud<PointT>& local_cloud,
+    const Eigen::Vector3d& local_grav,
+    util::geom::Pose3f& pose )
+{
+    std::unique_lock lock{ this->mtx };
+    DetectionStatus status{ 0 };
+
+    this->in_cloud.clear();
+    this->in_cloud.reserve(local_cloud.size());
+
+    const float squared_range_thresh =
+        static_cast<float>(this->param.point_range_thresh * this->param.point_range_thresh);
+    for(const PointT& pt : local_cloud)
+    {
+        if( pt.reflective > 0.f &&
+            pt.getVector3fMap().squaredNorm() <= squared_range_thresh )
+        {
+            this->in_cloud.transient_emplace_back(pt.x, pt.y, pt.z);
+        }
+    }
+
+    this->in_cloud.height = 1;
+    this->in_cloud.width = this->in_cloud.points.size();
+    this->in_cloud.is_dense = true;
+
+    if(this->in_cloud.size() < this->param.num_points_thresh) return status;
+
+    util::voxel_filter(
+        this->in_cloud,
+        this->seg_clouds[0],
+        Eigen::Vector3f::Constant(this->param.vox_filter_res) );
+
+    const size_t starting_num_points = this->seg_clouds[0].size();
+    if(starting_num_points < this->param.num_points_thresh) return status;
+
+    status.has_point_num = true;
+
+    this->seg.setModelType(pcl::SACMODEL_PARALLEL_PLANE);
+    this->seg.setInputCloud(this->seg_cloud_ptrs[0]);
+    this->seg.setAxis(local_grav.template cast<float>());
+
+    for(size_t iter = 0; iter < 2; iter++)
     {
         this->seg_indices.indices.clear();
         this->plane_coeffs.values.clear();
