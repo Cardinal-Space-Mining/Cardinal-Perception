97819f729f8b70448418263149e7731af4aa70d0
S1ink <samrichter888@gmail.com>
2025-09-10 00:54:50 -0500

    screw it launch_utils is now a required launch dep

--- launch/perception.launch.py
+++ launch/perception.launch.py
@@ -17,30 +17,18 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 try:
     sys.path.append(os.path.join(get_package_share_directory('launch_utils'), 'src'))
     from launch_utils.preprocess import preprocess_launch_json
-    from launch_utils.common import try_load_json, parse_launch_args
-    from launch_utils import utilities as lu
+    from launch_utils.actions import get_util_actions
+    from launch_utils.common import try_load_json_from_args, parse_launch_args, flatten_dict
     print('SUCCESSFULLY LOADED LAUNCH UTILS')
-    have_lu = True
+    HAVE_LAUNCH_UTILS = True
 except Exception as e:
     print('FAILED TO LOAD LAUNCH UTILS')
-    have_lu = False
+    HAVE_LAUNCH_UTILS = False
 
 
 PKG_PATH = get_package_share_directory('cardinal_perception')
 DEFAULT_JSON_PATH = os.path.join(PKG_PATH, 'config', 'perception.json')
 
-
-def try_load_json(json_path, default_json_path = ''):
-    if not json_path:
-        json_path = default_json_path
-    try:
-        with open(json_path, 'r') as f: json_data = f.read()
-    except Exception as e:
-        raise RuntimeError(f"JSON file '{json_path}' does not exist or could not be read : {e}")
-    try:
-        return json.loads(json_data)
-    except Exception as e:
-        raise RuntimeError(f"Failed to load json data from file '{json_path}' : {e}")
 
 def flatten_exclusion_zones(config):
     if 'exclusion_zones' in config:
@@ -70,73 +58,82 @@ def flatten_streams(config):
         config['streams'] = { 'num_streams' : 0 }
 
 def flatten_tags(config):
-    if 'tags' in config:
+    if 'aruco' in config and 'tags' in config['aruco']:
         flattened_tags = {}
         flattened_tags['ids'] = []
-        for tag in config['tags']:
+        for tag in config['aruco']['tags']:
             id = tag['id']
             flattened_tags['ids'].append(id)
             flattened_tags[f'tag{id}_static'] = tag['static']
             flattened_tags[f'tag{id}_frames'] = tag['frames']
             flattened_tags[f'tag{id}_corners'] = tag['corners']
+        config['aruco']['tags'] = flattened_tags
     else:
-        config['tags'] = {'ids':[]}
+        if 'aruco' not in config:
+            config['aruco'] = {}
+        config['aruco']['tags'] = {'ids':[]}
 
-def flatten_dict(d, parent_key='', sep='.'):
-    items = {}
-    for k, v in d.items():
-        new_key = f"{parent_key}{sep}{k}" if parent_key else k
-        if isinstance(v, dict):
-            items.update(flatten_dict(v, new_key, sep=sep))
-        else:
-            items[new_key] = v
-    return items
+def preproc_perception_config(config):
+    flatten_exclusion_zones(config)
+
+def preproc_tag_detector_config(config):
+    flatten_streams(config)
+    flatten_tags(config)
+
+def get_perception_actions(config):
+    actions = []
+    if 'perception' in config:
+        percept_config = config['perception']
+        preproc_perception_config(percept_config)
+        actions.append(
+            Node(
+                name = 'perception',
+                package = 'cardinal_perception',
+                executable = 'perception_node',
+                output = 'screen',
+                parameters = [flatten_dict(percept_config)],
+                remappings = [('/trace_notifications', '/cardinal_perception/trace_notifications')]
+            ) )
+        actions.append(
+            Node(
+                name = 'perception_profiling_manager',
+                package = 'csm_metrics',
+                executable = 'profiling_manager.py',
+                output = 'screen',
+                parameters = [{'notification_topic': '/cardinal_perception/trace_notifications'}]
+            ) )
+    if 'tag_detection' in config:
+        tag_det_config = config['tag_detection']
+        preproc_tag_detector_config(tag_det_config)
+        actions.append(
+            Node(
+                name = 'tags_detector',
+                package = 'cardinal_perception',
+                executable = 'tag_detection_node',
+                output = 'screen',
+                parameters = [flatten_dict(tag_det_config)]
+            )
+        )
+    return actions
 
 
 def launch(context, *args, **kwargs):
-    json_path = str(LaunchConfiguration('json_path').perform(context))
-    json_data = str(LaunchConfiguration('json_data').perform(context))
-
-    if not json_data:
-        json_data = try_load_json(json_path, DEFAULT_JSON_PATH)
-
     actions = []
 
-    if have_lu:
+    if HAVE_LAUNCH_UTILS:
         launch_args = parse_launch_args(context.argv)
+        json_data = try_load_json_from_args(launch_args, DEFAULT_JSON_PATH)
         config = preprocess_launch_json(json_data, launch_args)
-        for k, _ in config.items():
-            print(f"Loaded action config for '{k}'")
-        # if a non-preproc scope was passed (and no preprocessing occurred), we DO NOT want to do this in case there are accidentally matching tags!
-        actions.extend(lu.get_util_actions(config))
-        percept_config = config['perception'] if 'perception' in config else {}
+        if config is not json_data:
+            actions.extend(get_util_actions(config))
+        actions.extend(get_perception_actions(config))
     else:
-        if 'perception' in json_data:
-            for k, v in json_data['perception'].items():
-                if isinstance(v, dict):
-                    percept_config = v
-                    break
-        else:
-            percept_config = json_data
-
-    flatten_exclusion_zones(percept_config)
-    actions.append(
-        Node(
-            name = 'perception',
-            package = 'cardinal_perception',
-            executable = 'perception_node',
-            output = 'screen',
-            parameters = [flatten_dict(percept_config)]
-        )
-    )
+        print("To properly launch Cardinal Perception the 'launch_utils' package is needed!")
 
     return actions
 
 
-
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument('json_path', default_value=''),
-        DeclareLaunchArgument('json_data', default_value=''),
         OpaqueFunction(function=launch),
     ])
